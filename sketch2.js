1490d5f8d8c7a3f4fdd7094ccaf91142c9f4582a
Luckystardance <mintliliez@gmail.com>
2016-09-06 08:42:17 -0500

    Please be done

---
+++ sketch2.js
@@ -0,0 +1,101 @@
+// Melinda Buckner #19
+
+function setup () {
+  createCanvas(400,269);
+  background(25);
+
+  fill(245,189,5);
+  strokeWeight(0);
+  quad(0,0,0,130,155,130,55,0);
+
+  fill(0);
+  ellipse(56,20,40,30);
+
+  fill(0);
+  ellipse(120,105,48,40);
+
+  fill(0);
+  ellipse(70,130,50,30);
+
+  fill(0);
+  ellipse(60,50,30,25);
+
+  stroke(0);
+  strokeWeight(16);
+  line(35,20,45,60);
+
+  stroke(0);
+  strokeWeight(10);
+  line(80,50,100,70);
+
+  fill(1,7,63);
+  strokeWeight(0);
+  quad(50,0,170,150,400,150,400,0);
+
+  fill(156,11,11);
+  strokeWeight(8);
+  triangle(330,155,190,0,250,0);
+
+  stroke(0);
+  strokeWeight(13);
+  line(52,0,160,130);
+
+  fill(156,11,11);
+  strokeWeight(0);
+  rect(0,130,400,160);
+
+  stroke(156,11,11);
+  strokeWeight(8);
+  line(200,0,244,0);
+
+  fill(0);
+  strokeWeight(0);
+  rect(0,200,400,7);
+
+  fill(0);
+  rect(0,166,400,8);
+
+  fill(0);
+  rect(0,126,400,6);
+
+  fill(175);
+  strokeWeight(0);
+  triangle(382,0,400,0,400,19);
+
+  stroke(0);
+  strokeWeight(9);
+  line(5,15,20,65);
+
+  stroke(245,189,5);
+  strokeWeight(5);
+  line(0,0,10,50)
+
+  stroke(0);
+  strokeWeight(8);
+  line(385,0,400,16);
+
+  stroke(0);
+  strokeWeight(9);
+  line(18,0,20,20);
+
+  stroke(0);
+  strokeWeight(5);
+  line(15,100,30,126);
+
+  stroke(0);
+  strokeWeight(6);
+  line(8,80,25,110);
+
+  stroke(0);
+  strokeWeight(11);
+  line(12,65,23,90);
+
+  stroke(0);
+  strokeWeight(8);
+  line(15,90,15,11);
+
+  stroke(0);
+  strokeWeight(5);
+  line(17,11,19,45);
+
+}
